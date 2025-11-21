f3569923c29cf6071bcfd74d8aea974cbd515304
조윤지 <yunji8533@naver.com>
2025-11-21 16:50:43 +0900

    feat: app.js에 로그인 화면 추가

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-import { NavigationContainer, createNavigationContainerRef } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  createNavigationContainerRef,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ProjectListScreen from './src/screens/project/ProjectListScreen';
 import ProjectDetailScreen from './src/screens/project/ProjectDetailScreen';
+import LoginScreen from './src/screens/auth/LoginScreen';
 import NavigationBar from './src/components/NavigationBar';
 import ProjectAddButton from './src/components/ProjectAddButton';
 import Header from './src/components/HeaderBar';
@@ -23,27 +27,25 @@ export default function App() {
   return (
     <NavigationContainer ref={navigationRef}>
       <View style={{ flex: 1 }}>
-
         {!hideHeader && <Header />}
 
         {/* Stack 화면 */}
         <View style={{ flex: 1 }}>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login">
+              {props => (
+                <LoginScreen {...props} setHideHeader={setHideHeader} />
+              )}
+            </Stack.Screen>
             <Stack.Screen name="ProjectList">
-              {(props) => (
-                <ProjectListScreen 
-                  {...props}
-                  setHideHeader={setHideHeader}
-                />
+              {props => (
+                <ProjectListScreen {...props} setHideHeader={setHideHeader} />
               )}
             </Stack.Screen>
 
             <Stack.Screen name="ProjectDetail">
-              {(props) => (
-                <ProjectDetailScreen 
-                  {...props}
-                  setHideHeader={setHideHeader}
-                />
+              {props => (
+                <ProjectDetailScreen {...props} setHideHeader={setHideHeader} />
               )}
             </Stack.Screen>
           </Stack.Navigator>
